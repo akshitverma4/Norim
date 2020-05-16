9c66c95ebef8d915c5d727a9585a61dfa59b6f17
akshitverma4 <59332509+akshitverma4@users.noreply.github.com>
2020-05-16 10:52:46 +0530

    First commit

---
+++ app/src/main/java/com/example/android/norim/PageAdapter.kt
@@ -0,0 +1,45 @@
+package com.example.android.norim
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+
+
+class PageAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+    override fun getItem(position: Int): Fragment {
+        return when (position) {
+            0 -> {
+                NotesFragment()
+            }
+            1 -> {
+                RemindersFragment()
+            }
+            3 -> {
+                SettingsFragment()
+            }
+            else -> {
+               NotesFragment()
+            }
+        }
+
+    }
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        when (position) {
+            0 -> {
+                return "Notes"
+            }
+            1 -> {
+                return "Reminders"
+            }
+            2 -> {
+                return "Settings"
+            }
+        }
+        return super.getPageTitle(position)
+    }
+
+    override fun getCount(): Int {
+        return 3
+    }
+}
